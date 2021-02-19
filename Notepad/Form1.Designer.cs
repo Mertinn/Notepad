893605f3d39414011b69d2ae224f8375f9843c8d
Mertinn <marcin.rajs@wp.pl>
2021-02-19 17:01:37 +0100

    Added load button.

--- Notepad/Form1.Designer.cs
+++ Notepad/Form1.Designer.cs
@@ -29,13 +29,78 @@ namespace Notepad
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.loadBtn = new System.Windows.Forms.ToolStripButton();
+            this.saveBtn = new System.Windows.Forms.ToolStripButton();
+            this.documentTextBox = new System.Windows.Forms.TextBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadBtn,
+            this.saveBtn});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(783, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // loadBtn
+            // 
+            this.loadBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.loadBtn.Image = ((System.Drawing.Image)(resources.GetObject("loadBtn.Image")));
+            this.loadBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.loadBtn.Name = "loadBtn";
+            this.loadBtn.Size = new System.Drawing.Size(52, 22);
+            this.loadBtn.Text = "Wczytaj";
+            this.loadBtn.Click += new System.EventHandler(this.loadBtn_Click);
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.saveBtn.Image = ((System.Drawing.Image)(resources.GetObject("saveBtn.Image")));
+            this.saveBtn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(44, 22);
+            this.saveBtn.Text = "Zapisz";
+            this.saveBtn.Click += new System.EventHandler(this.saveBtn_Click);
+            // 
+            // documentTextBox
+            // 
+            this.documentTextBox.BackColor = System.Drawing.SystemColors.Control;
+            this.documentTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.documentTextBox.Location = new System.Drawing.Point(13, 29);
+            this.documentTextBox.Multiline = true;
+            this.documentTextBox.Name = "documentTextBox";
+            this.documentTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.documentTextBox.Size = new System.Drawing.Size(758, 454);
+            this.documentTextBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(783, 495);
+            this.Controls.Add(this.documentTextBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton loadBtn;
+        private System.Windows.Forms.ToolStripButton saveBtn;
+        private System.Windows.Forms.TextBox documentTextBox;
     }
 }
 
--- Notepad/Form1.cs
+++ Notepad/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,39 @@ namespace Notepad
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadBtn_Click(object sender, EventArgs e)
+        {
+            string filePath = OpenExplorerAndGetPath();
+            if(filePath != string.Empty)
+            {
+                string documentText = File.ReadAllText(filePath);
+                documentTextBox.Text = documentText;
+            }
+        }
+
+        private void saveBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private string OpenExplorerAndGetPath()
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\Desktop";
+                openFileDialog.Filter = "txt (*.txt)|*.txt";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                string filePath = string.Empty;
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                   filePath = openFileDialog.FileName;
+                }
+                return filePath;
+            }
         }
     }
 }
